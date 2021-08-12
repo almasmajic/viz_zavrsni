d6554333cbe31163e25b37fa7b0613575a911018
Alma Smajic <53364813+almasmajic@users.noreply.github.com>
2021-08-13 01:33:06 +0200

    Removing unnecessary libs

--- Bar_chart.py
+++ Bar_chart.py
@@ -1,19 +1,12 @@
 # In[1]:
 import pandas as pd
 
-# In[2]:
-import numpy as np
-
-# In[3]:
-import matplotlib.pyplot as plt
-
 # In[4]:
 import plotly.graph_objs as go
 
 # In[5]:
 from plotly.offline import iplot
-# In[6]:
-import seaborn as sns
+
 # In[7]:
 df = pd.read_csv(r'\Users\almas\Desktop\Tokyo Medals 2021.csv')
 # In[8]:
